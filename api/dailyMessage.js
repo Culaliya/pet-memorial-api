1261cf361a0caf06e6939814699a1f4dc999ca28
Culaliya <culaliya@gmail.com>
2025-10-27 11:41:21 +0800

    Update dailyMessage.js

--- api/dailyMessage.js
+++ api/dailyMessage.js
@@ -1,1 +1,10 @@
+export default function handler(req, res) {
+  const messages = [
+    "今天的風也有你的味道。",
+    "睡著的樣子，還是一樣可愛。",
+    "有光的地方，我都想起你。"
+  ];
+  const pick = messages[Math.floor(Math.random() * messages.length)];
+  res.status(200).json({ message: pick });
+}
 
